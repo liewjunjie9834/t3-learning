85608acc91b5d0018cafdbc8ff21fac8ba6301ae
liewjunjie9834 <81868348+liewjunjie9834@users.noreply.github.com>
2024-01-13 20:54:31 +0800

    updated import page

--- src/app/project/import/_components/configure-project-container.tsx
+++ src/app/project/import/_components/configure-project-container.tsx
@@ -1,13 +1,211 @@
-import React from "react";
+import React from 'react'
+
+
 
 
 const ConfigureProjectContainer = () => {
 
     return (
+    <div className=" mt-4 w-full h-full">
+      <div className=" container ">
+        <div className="w-full">
+          <img src="./../images/Dummy_Logo (1).png" className="mx-auto" alt="" />
+          <p className=" text-start text-2xl text-neutral-600 text-font-bold font-semibold mt-4 ml-10">Configure Project</p>
+          <div className=" border h-0 w-11/12 mx-auto mt-5 border-stone-300"></div>
+          <div/>
+          <div className="mt-8">
+            <form action="" className="px-10">
+              <div className="">
+                <label htmlFor="" className="text-neutral-600 text-sm text-base font-normal">Project Name</label>
+                <div className=" items-center justify-between border border-gray-300 bg-gray-50 rounded-md mt-2">
+                  <input
+                    type="text"
+                    name=""
+                    placeholder=""
+                    id=""
+                    className="w-full h-10 text-neutral-600 placeholder:text-neutral-600 px-4 bg-transparent outline-none"
+                  />
+                </div>
+              </div>
+              <div className="mt-4">
+                <label htmlFor="" className="text-neutral-600 text-sm text-base font-normal ">Framework Preset</label>
+                <div className="items-center justify-between border border-gray-300 bg-gray-50 rounded-md mt-2">
+                  <input
+                    type="password"
+                    name=""
+                    placeholder=""
+                    id=""
+                    className="w-full h-10 text-neutral-600 placeholder:text-neutral-600 px-4 bg-transparent outline-none"
+                  />
+                </div>
+              </div>
+              <div className="mt-4 ">
+                <label htmlFor="" className="text-neutral-800 text-sm text-base font-normal ">Root Directory</label>
+                <div className=" flex items-center justify-between border border-gray-300 bg-gray-50 rounded-tl-md rounded-bl-md rounded-tr-xl rounded-br-xl mt-2">
+                  <input
+                    type="text"
+                    name=""
+                    placeholder=""
+                    id=""
+                    className="w-9/12 h-9 text-neutral-600 placeholder:text-neutral-600 px-4 bg-transparent outline-none"/>
+                    <div className="border border-gray-300  hover:bg-gray-50  py-2 px-3  rounded-xl ">
+                    <button className="text-md " >
+                      <a href=" ">Edit</a>
+                    </button>
+                    </div>
+                </div>
+              </div>
+           <div className="pt-6 gap-4">
+              <div className=" border border-gray-200 rounded-md py-4">
+			<details className="group">
+				<summary className="flex justify-between items-center font-medium cursor-pointer list-none ml-3">
+					<span className="text-sm text-gray-900">Build and output settings</span>
+					<span className="transition group-open:rotate-180 mr-3">
+                <svg fill="none" height="24" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path>
+                 </svg>
+              </span>
+				</summary>
+                <div className="flex gap-4 ">
+                    <div className=" mt-4">
+				<label htmlFor="" className=" text-neutral-800 text-sm text-base font-normal ml-4 mt-2  ">Key</label>
+                
+                <div className="  justify-between border border-gray-300 bg-gray-50 rounded-md w- h-10 mt-2 ml-4">
+                  <input
+                    type="text"
+                    name=""
+                    placeholder=""
+                    id=""
+                    className=" text-neutral-600 placeholder:text-neutral-600 px-4 bg-transparent outline-none"
+                  />
+                </div>
+                </div>
+                 <div className="mt-4">
+                <label htmlFor="" className=" text-neutral-800 text-sm text-base font-normal  mt-2  ">Value(will be Encrypted)</label>
+                <div className=" items-center justify-between border border-gray-300 bg-gray-50 rounded-md  h-10 mt-2 ">
+                  <input
+                    type="text"
+                    name=""
+                    placeholder=""
+                    id=""
+                    className=" text-neutral-600 placeholder:text-neutral-600 px-4 bg-transparent outline-none"
+                  />
+                </div>
+                </div>
+                
+                    <button className=" border border-gray-300 mt-11 mr-6 ml-4 justify center hover:bg-gray-50 w-1/6 h-11  rounded-xl text-md " >
+                      <a href=" ">Edit</a>
+                    </button>
+                </div>
+			</details>
 
-        <div>
-            JJ
+            </div>
+
+                
+            <div className=" border border-gray-200 rounded-md mt-4 py-4">
+			<details className="group">
+				<summary className="flex justify-between items-center font-medium cursor-pointer list-none ml-3 ">
+					<span className="text-sm text-gray-900">Enviroment Variables</span>
+                    <span className="transition group-open:rotate-180 mr-3">
+                <svg fill="none" height="24" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path>
+                 </svg>
+              </span>
+					
+				</summary>
+
+                <label htmlFor="" className=" text-neutral-800 text-sm text-base font-normal ml-4 mt-2  ">Build Command</label>
+                <div className="flex ">
+                <div className=" flex items-center justify-between border border-gray-300 bg-gray-50 rounded-md w-3/4 h-10 mt-2 ml-4">
+                  <input
+                    type="text"
+                    name=""
+                    placeholder=""
+                    id=""
+                    className=" text-neutral-600 placeholder:text-neutral-600 px-4 bg-transparent outline-none"
+                  />
+                </div>
+				<p className="text-sm mt-4 ml-2">OVERRIDE</p>
+
+                <label className='flex cursor-pointer select-none items-center ml-2'>
+                <div className='relative'>
+          <input
+            type='checkbox'
+            className='sr-only'
+          />
+          <div className='block h-8 w-14 rounded-full bg-[#E5E7EB]'></div>
+          <div className='dot absolute left-1 top-1 h-6 w-6 rounded-full bg-white transition'></div>
         </div>
+        </label>
+                
+                </div>
+
+                <label htmlFor="" className=" text-neutral-800 text-sm text-base font-normal ml-4 mt-2  ">Output Directory</label>
+                <div className="flex ">
+                <div className=" flex items-center justify-between border border-gray-300 bg-gray-50 rounded-md w-3/4 h-10 mt-2 ml-4">
+                  <input
+                    type="text"
+                    name=""
+                    placeholder=""
+                    id=""
+                    className=" text-neutral-600 placeholder:text-neutral-600 px-4 bg-transparent outline-none"
+                  />
+                </div>
+				<p className="text-sm mt-4 ml-2">OVERRIDE</p>
+
+                <label className='flex cursor-pointer select-none items-center ml-2'>
+                <div className='relative'>
+          <input
+            type='checkbox'
+            className='sr-only'
+          />
+          <div className='block h-8 w-14 rounded-full bg-[#E5E7EB]'></div>
+          <div className='dot absolute left-1 top-1 h-6 w-6 rounded-full bg-white transition'></div>
+        </div>
+        </label>
+                
+                </div>
+
+                
+               
+                <label htmlFor="" className=" text-neutral-800 text-sm text-base font-normal ml-4 mt-2  ">Install Command</label>
+                <div className="flex ">
+                <div className=" flex items-center justify-between border border-gray-300 bg-gray-50 rounded-md w-3/4 h-10 mt-2 ml-4">
+                  <input
+                    type="text"
+                    name=""
+                    placeholder=""
+                    id=""
+                    className=" text-neutral-600 placeholder:text-neutral-600 px-4 bg-transparent outline-none"
+                  />
+                </div>
+				<p className="text-sm mt-4 ml-2">OVERRIDE</p>
+
+                <label className='flex cursor-pointer select-none items-center ml-2'>
+                <div className='relative'>
+          <input
+            type='checkbox'
+            className='sr-only'
+          />
+          <div className='block h-8 w-14 rounded-full bg-[#E5E7EB]'></div>
+          <div className='dot absolute left-1 top-1 h-6 w-6 rounded-full bg-white transition'></div>
+        </div>
+        </label>
+                
+                </div>
+			</details>
+
+            </div>
+
+              </div>
+              <button className="bg-black rounded-lg shadow text-center text-white text-base font-semibold w-full py-2 mt-5">
+                Deploy
+              </button>
+
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  
 
 
 
--- src/app/project/import/page.tsx
+++ src/app/project/import/page.tsx
@@ -1,38 +1,43 @@
 import List from "./_components/deploy-list";
 import NavigationBar from "~/app/_components/navigation-bar";
 import ConfigureProjectContainer from "./_components/configure-project-container";
+import React, { useState } from 'react'
 
 
 const Import =() => {
  
     return (
-    <div className="justify-center">
+    <div className="">
         <div>
             <NavigationBar/>
         </div>
-        <div className="container w-1/2 h-20 pt-24 pb-16 mx-auto justify-center mb-10 gap-4 ">
-              <button className=" hover:bg-gray-200 mb-3  py-1 px-3  rounded-full  ">
+        <div className="container w-1/2 h-20 pt-16 pb-24 mx-auto mb-10 gap-6 ">
+              <button className=" hover:bg-gray-50 mb-4  py-1 px-3  rounded-full  ">
                 <a href=" ">Back</a>
-
               </button>
 
-              <p className="text-5xl font-black ">
+              <p className="text-4xl font-black mb-4 ">
                 You're almost done.
               </p>
 
-              <p className="text-xl text-base ">
+              <p className="text-md text-base mb-6">
                  Please follow the step configure your project and deploy it.
               </p>
         </div>
 
  
-        <div className="border border-solid border-gray-700 shadow-2xl rounded-xl bg-grey-100 py-24 mt-20 w-1/2 h-96 mx-auto">
-        <List/>
-        </div>
-
-        <div className="border border-solid border-gray-700 shadow-2xl rounded-xl bg-grey-100 py-24 mt-20 w-1/2 h-96 mx-auto">
+        <div className="border border-solid border-gray-300 shadow-xl rounded-md bg-grey-50 w-5/12 h-full pb-10 mx-auto mt-20">
         <ConfigureProjectContainer/>
         </div>
+          <div className="border border-solid border-gray-300 shadow-xl rounded-md bg-grey-50 py-24 mx-auto mt-20 mb-10 w-5/12 h-96 ">
+        </div>
+
+
+        
+    
+  
+
+
 
         
 
